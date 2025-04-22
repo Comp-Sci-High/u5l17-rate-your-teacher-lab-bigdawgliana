8c77ed20524ecdcdae90a6368b7fe1fb178ad130
bigdawgliana <liana.ortiz25@compscihigh.org>
2025-04-22 19:01:49 +0000

    I finished

---
+++ public/ratingScript.js
@@ -0,0 +1,19 @@
+// Add an event listener to the form that upon submits creates a new rating
+// When the rating is created redirect to the ratings page
+
+const createForm = document.querySelector("form")
+
+createForm.addEventListener("submit", async(e)=> {
+    e.preventDefault()
+
+    const newRating = new FormData(createForm)
+    const reqBody = Object.fromEntries(newRating)
+
+    const response = await fetch("/add/rating", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(reqBody)
+    })
+})
---
+++ public/teacherScript.js
@@ -0,0 +1,15 @@
+const createForm = document.querySelector("form")
+
+createForm.addEventListener("submit", async(e)=> {
+    e.preventDefault()
+    const newTeacher = new FormData(createForm)
+    const reqBody = Object.fromEntries(newTeacher)
+
+    const response = await fetch("/add/teacher", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(reqBody)
+    })
+})
